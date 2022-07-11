ccf8fb8736a463931ddc34ef32d6ba1dd13f207c
jfkneib <jean-francois.kneib@siveo.net>
2022-07-11 14:40:44 +0200

    [py3] add information version et build in connectionagent

--- pulse_xmpp_agent/connectionagent.py
+++ pulse_xmpp_agent/connectionagent.py
@@ -84,6 +84,7 @@ from lib.utils import (
     geolocalisation_agent,
     serialnumbermachine,
     base64strencode,
+    search_system_info_reg,
 )
 
 from optparse import OptionParser
@@ -711,6 +712,7 @@ class MUCBot(ClientXMPP):
                 "agent_machine_name": self.agent_machine_name,
                 "uuid_serial_machine": serialnumbermachine(),
                 "regcomplet": self.FullRegistration,
+                "system_info" : search_system_info_reg(),
                 }
 
 
